3f0839e567c04e954de608b11c7554e0b76d3351
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 21:59:08 +0100

    Add sorted variable & return results

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,8 +1,12 @@
 def bubble_sort(array)
   # declare variable if the array is sorted
+  sorted = false
   # create a loop
   # compare every pair
+
   # return results
+  p array
+  array
 end
 
 bubble_sort([4,3,78,2,0,2])
