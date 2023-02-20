a91d5b5cb071a73eab0330fb6518cda8944fe998
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 18:59:05 +0100

    Display days

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -2,7 +2,12 @@ prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(prices)
   # display days
+  days = [0, 0]
   # calculate the most profit
+
+
+  p days
+  return days
 end
 
 stock_picker(prices)
