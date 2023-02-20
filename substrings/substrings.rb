43b2db481b34199cb13c0decf95f94132d170216
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-02-20 18:22:34 +0100

    Count matches

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,8 +1,13 @@
 def substrings(str, dictionary)
+  # case-insensitive input
+  str = str.downcase
   # display results in hash
   result = Hash.new(0)
   # handles multiple words
+  dictionary.each do |word|
   # count amount of matches
+    matches = str.scan(word).count
+  end
 
   return result
 end
